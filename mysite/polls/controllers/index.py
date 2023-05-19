0c081a2c6e4a0051cc9b24b8d048d090a957dd9d
Valeriya Bondar <valeriabnd@mail.ru>
2023-05-19 17:48:44 +0300

    Initial commit

---
+++ mysite/polls/controllers/index.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+def index(request):
+    return render(request, 'index.html', {})
+
+def frames(request):
+    return render(request, 'frames.html', {})
+
+def sunglasses(request):
+    return render(request, 'sunglasses.html', {})
+
+def lenses(request):
+    return render(request, 'lenses.html', {})
+
+def contacts(request):
+    return render(request, 'contacts.html', {})
